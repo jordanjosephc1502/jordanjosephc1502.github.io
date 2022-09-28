2a6563a7d3a26a91151d1c63491a84731e99e7e7
jordan <114585690+jordanjosephc1502@users.noreply.github.com>
2022-09-28 08:35:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "jordan"; // update the part BETWEEN the "quotes"
+let userName = "jordanjosephc1502"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
